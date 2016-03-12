dfb6ce0989415ffdddf747699b49c2accfb8b910
Sandra <sandrabasquero@gmail.com>
2016-03-12 19:41:21 +0100

    Sincronizando Modelo y Vista

--- Baccus/Baccus/SBSWineViewController.h
+++ Baccus/Baccus/SBSWineViewController.h
@@ -11,7 +11,19 @@
 
 @interface SBSWineViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *wineryNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *typeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *originLabel;
+@property (weak, nonatomic) IBOutlet UILabel *grapesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *notesLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+@property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *ratingViews;
+
+
 @property (strong, nonatomic) SBSWineModel *model;
 -(id) initWithModel: (SBSWineModel*) aModel;
+
+- (IBAction)displayWeb:(id)sender;
 
 @end
